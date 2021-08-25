35ef2d36b67d2e03e118546a32ab56295a664285
tiagorahal <rahal.aires@gmail.com>
2021-08-25 11:08:39 -0300

    Change style file for calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-import '../App.css';
+import './calculator.css';
 
 const Calculator = () => {
   const [total, setTotal] = useState(null);
