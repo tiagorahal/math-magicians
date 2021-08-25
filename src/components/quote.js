381afcec0fb0cedf726758b1dd6f039b25c6d7cf
tiagorahal <rahal.aires@gmail.com>
2021-08-25 11:13:03 -0300

    Add quote.js and quote.css

---
+++ src/components/quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './quote.css';
+
+const Quote = () => (
+  <main className="quote-content">
+    <p className="text-background">
+      Mathematics is not about numbers, equations, computations or algorithms:
+      it is about understanding.
+    </p>
+    <p className="quote-author text-background">William Paul Thurson</p>
+  </main>
+);
+
+export default Quote;
