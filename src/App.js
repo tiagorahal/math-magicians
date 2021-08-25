443e09396484f29a8fd80e103deb2aef8e519e10
tiagorahal <rahal.aires@gmail.com>
2021-08-25 11:16:30 -0300

    Add index.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Calculator from './components/calculator';
+import Navbar from './components/navbar';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+  </div>
+);
 
 export default App;
