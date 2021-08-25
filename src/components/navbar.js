3ac84e621d9136c95318a1509dd095f2866316df
tiagorahal <rahal.aires@gmail.com>
2021-08-25 11:11:03 -0300

    Add navbar.js and navbar.css

---
+++ src/components/navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Home from './home';
+import Calculator from './calculator';
+import Quote from './quote';
+import './navbar.css';
+
+const Navbar = () => (
+  <Router>
+    <header className="navbar-content">
+      <h1>Math Magicians</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </header>
+
+    <Switch>
+      <Route path="/calculator">
+        <Calculator />
+      </Route>
+      <Route path="/quote">
+        <Quote />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default Navbar;
