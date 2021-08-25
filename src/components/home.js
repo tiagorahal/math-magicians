4710cb2d1810c75dbbd96b449921417906d02c12
tiagorahal <rahal.aires@gmail.com>
2021-08-25 11:09:26 -0300

    Add home.js

---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => (
+  <main className="home-content">
+    <h2>Welcome to Our Page</h2>
+    <p className="text-background">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus pharetra efficitur ligula, eu pulvinar quam rhoncus at.
+      Etiam molestie tempor laoreet.
+    </p>
+    <p className="text-background">
+      Mauris elit nisl, feugiat sit amet porta ac,
+      vehicula nec velit. Proin efficitur tortor lectus, a malesuada mi consectetur a.
+      Sed libero dolor, tempus et commodo vitae, ultricies at nunc. Etiam vitae tristique turpis.
+      Cras ultrices blandit risus, in convallis est laoreet nec.
+      Sed sollicitudin, massa sit amet varius bibendum, nisi metus volutpat metus,
+      sit amet lobortis diam nisl ut felis.
+    </p>
+    <p className="text-background">
+      Suspendisse euismod nulla quis dolor porttitor accumsan.
+      Aenean eget mauris vel justo imperdiet tincidunt.
+      Sed in turpis venenatis ligula aliquam sagittis et eu orci.
+      Cras vestibulum sed mauris vel tristique.
+      Integer in libero auctor nunc semper volutpat. Nunc suscipit tincidunt nibh,
+      id iaculis arcu posuere et. Cras eu nisi ex. Vestibulum quis luctus sapien.
+      Mauris tincidunt est nec libero commodo faucibus. Maecenas a aliquam eros.
+    </p>
+  </main>
+);
+
+export default Home;
