72eac59d3732b8147e63124e773277759d569242
tiagorahal <rahal.aires@gmail.com>
2021-08-26 15:51:57 -0300

    Add compenents tests

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Quote = () => (
   <div className="quote-container">
     <h3>
-    Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. - William Paul Thurston
+      Mathematics is not about numbers, equations, computations, or algorithms: it is about
+      understanding.
+      <br />
+      - William Paul Thurston
     </h3>
   </div>
 );
