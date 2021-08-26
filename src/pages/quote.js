ed29ac07e69f7f34c26a3b9df450077c3dd0138b
tiagorahal <rahal.aires@gmail.com>
2021-08-26 17:51:51 -0300

    Fix quote test issue

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -5,7 +5,6 @@ const Quote = () => (
     <h3>
       Mathematics is not about numbers, equations, computations, or algorithms: it is about
       understanding.
-      <br />
       - William Paul Thurston
     </h3>
   </div>
